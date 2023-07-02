abf4d2ea631bc3e9af1194011a7bc8b132d3526d
Bishal De <itsbishalde@gmail.com>
2023-07-02 10:34:53 +0530

    fixed-files

--- Shrinkk/settings.py
+++ Shrinkk/settings.py
@@ -74,11 +74,21 @@ WSGI_APPLICATION = 'Shrinkk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'ShrinkkDatabase',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'mongodb+srv://bishalde:bishalde@shrinkkdatabase.odms2ms.mongodb.net/?retryWrites=true&w=majority'
+            }  
+        }
 }
 
 
